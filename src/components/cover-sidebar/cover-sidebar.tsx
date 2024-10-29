b7395bf532431b686bcb1f3a8646fb1a0fc34451
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-29 02:16:17 -0300

    🚀✨ Add sidebar component

---
+++ src/components/cover-sidebar/cover-sidebar.tsx
@@ -0,0 +1,51 @@
+import { cva, VariantProps } from 'class-variance-authority'
+import { Icon } from '../icon/icon'
+import style from './cover-sidebar.module.css'
+
+const sidebarVariants = cva(
+  style.sidebar,
+  {
+    variants: {
+      inverse: {
+        true: style.inverse,
+        false: style.base
+      },
+      bottom: {
+        true: style.bottom,
+        false: ''
+      }
+    },
+    defaultVariants: {
+      inverse: false,
+      bottom: false
+    }
+  }
+)
+
+interface SidebarProps extends VariantProps<typeof sidebarVariants> {
+  className?: string
+  inverse?: boolean
+  bottom?: boolean
+}
+
+export function Sidebar({ className, inverse, bottom }: SidebarProps) {
+  const classes = className
+    ? `${sidebarVariants({ inverse, bottom })} ${className}`
+    : sidebarVariants({ inverse, bottom })
+
+  return (
+    <div className={classes}>
+      <div className={style.vl} />
+
+      <div className={style.icons}>
+        <Icon className={style['side-icon']} size={32} type='linkedin' inverse={inverse} />
+        <Icon className={style['side-icon']} size={32} type='github' inverse={inverse} />
+      </div>
+
+      <div className={style['bottom-container']}>
+        <div className={style.box} />
+        <Icon className={style['bottom-icon']} type='down-arrow' size={28} inverse={inverse} />
+      </div>
+    </div>
+  )
+}
