6dcf7e124428d089f14eab3424e3f0f0e9136d7f
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-30 14:13:14 -0300

    🚀 Add profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+// import { Sidebar } from './components/sidebar/sidebar'
+// import { Header } from './components/header/header'
+
 import { Cover } from './pages/cover/cover'
+import { Profile } from './pages/profile/profile'
 import './App.css'
 
 function App() {
   return (
     <div className='app'>
       <Cover />
-      <div className='page'>
-        <h1>Hello World</h1>
-      </div>
+      <Profile />
+
+      {/* <main className='app-grid'>
+        <Header className='page-header' title='Profile' />
+        <Sidebar className='page-sidebar' />
+
+        <div className='page-content'>
+          
+        </div>
+      </main> */}
     </div>
   )
 }
