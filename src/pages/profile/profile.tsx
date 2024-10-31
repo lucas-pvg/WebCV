afa442dc3f783f17b324fcb446b84f3f79d7dbe6
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-31 17:22:04 -0300

    ✨ Add profile to Education, Experience, Skill and Profile pages

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,3 +1,5 @@
+import { useContext } from 'react'
+import { ProfileContext } from '../../context/ProfileContext'
 import { Page } from '../page/page'
 import { ProfilePic } from '../../components/profile-pic/profile-pic'
 import { SectionTitle } from '../../components/section-title/section-title'
@@ -5,15 +7,20 @@ import { SectionTitle } from '../../components/section-title/section-title'
 import style from './profile.module.css'
 
 export function Profile() {  
+  const profile = useContext(ProfileContext)
+
   return (
     <div id='profile' className={[style.profile, 'page'].join(' ')}>
       <Page title='Profile' >
-        <ProfilePic />
+        <ProfilePic name={profile.name} subtitles={profile.subtitles} />
 
         <section className={style.text}>
           <SectionTitle title='About Me' />
-          <p>Tenho 21 anos e sou apaixonado por programação, desenvolvimento front-end e UX/UI design. Atualmente curso engenharia da computação na Escola Politécnica da USP enquanto procuro complementar minha formação, de maneira mais focada em minhas áreas de interesse, com projetos, cursos e outras oportunidades.</p>
-          <p>Também sou apaixonado por música e participei de uma orquestra, nos meus ensinos fundamental e médio, durante 7 anos. A música faz parte da minha rotina, de modo que, quando não estou tocando ou aprendendo algum instrumento, estou ouvindo ou pesquisando sobre. Atualmente, toco saxofone tenor, um pouco de flauta transversal e estou tentando aprender teclado.</p>
+          {
+            profile.about.map((text, index) => { return (
+              <p key={index}>{ text }</p>
+            )})
+          }
         </section>
       </Page>
     </div>
--- src/pages/skills/skills.tsx
+++ src/pages/skills/skills.tsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react'
+import { ProfileContext } from '../../context/ProfileContext'
 import { Page } from '../page/page'
 import { SkillTable } from '../../components/skill-table/skill-table'
 
 import style from './skills.module.css'
 
-export function Skills() {  
+export function Skills() { 
+  const profile = useContext(ProfileContext)
+
   return (
     <div id='skills' className={[style.skills, 'page'].join(' ')}>
       <Page 
@@ -11,9 +15,25 @@ export function Skills() {
         description="Here's a summary of the most import skills I developed with my experience as a front-end developer and web designer."
       >
         <section className={style.content}>
-          <SkillTable className={style.tech} label='Technologies' />
-          <SkillTable className={style.tools} label='Tools / Softwares' />
-          <SkillTable className={style.lang} label='Languages' />
+          <SkillTable 
+            className={style.tech}
+            label='Technologies'
+            data={profile.skills.filter(sk => sk.category == 'technology')}
+            rating
+          />
+
+          <SkillTable 
+            className={style.tools}
+            label='Tools / Softwares'
+            data={profile.skills.filter(sk => sk.category == 'tool')}
+            rating
+          />
+
+          <SkillTable
+            className={style.lang}
+            label='Languages'
+            data={profile.skills.filter(sk => sk.category == 'language')} 
+          />
         </section>
       </Page>
     </div>
