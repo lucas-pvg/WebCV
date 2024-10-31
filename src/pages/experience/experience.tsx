f21537a1fba219fd19b63468008c3ef96a0492a2
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-30 23:12:41 -0300

    🚀 Add experience page

---
+++ src/pages/experience/experience.tsx
@@ -0,0 +1,21 @@
+import { Page } from '../page/page'
+import { SectionTitle } from '../../components/section-title/section-title'
+import { Timeline } from '../../components/timeline/timeline'
+
+import style from './experience.module.css'
+
+export function Experience() {  
+  return (
+    <div id='experience' className={style.experience}>
+      <Page 
+        title='Experience'
+        description="Here's a timeline of my working experience up until now along with a summary of what I developed in each one of them."
+      >
+        <section className={style.content}>
+          <SectionTitle title='Career' />
+          <Timeline />
+        </section>
+      </Page>
+    </div>
+  )
+}
