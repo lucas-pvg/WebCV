09317be11175c14639bdeb170b1d639c337a90af
Lucas Garieri <lucaspvg1@gmail.com>
2024-10-31 17:23:59 -0300

    ✨ Cover page profile integration

--- src/pages/cover/cover.tsx
+++ src/pages/cover/cover.tsx
@@ -1,19 +1,24 @@
+import { useContext } from 'react'
+import { ProfileContext } from '../../context/ProfileContext'
 import { CoverNavbar } from '../../components/cover-navbar/cover-navbar'
 import { CoverTitle } from '../../components/cover-title/cover-title'
 import { Sidebar } from '../../components/sidebar/sidebar'
+
 import style from './cover.module.css'
 
 export function Cover() {
+  const profile = useContext(ProfileContext)
+
   return (
     <div id='cover' className={style.cover}>
       <CoverNavbar className={style['cover-navbar']} />
 
       <Sidebar className={style['cover-sidebar']} inverse bottom/>
 
-      <CoverTitle 
-        title='Lucas Pavan Garieri'
-        description='A React Front-End Developer and Web Designer'
+      <CoverTitle
         className={style['cover-title']}
+        title={profile.name}
+        description={profile.title}
       />
     </div>
   )
