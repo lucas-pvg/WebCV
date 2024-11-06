f4072e4227a7828cc00328e22849cd0f5edaa038
Lucas Garieri <lucaspvg1@gmail.com>
2024-11-06 04:19:28 -0300

    💫✨ Add exit animations to page

--- src/pages/page/page.tsx
+++ src/pages/page/page.tsx
@@ -10,15 +10,28 @@ interface PageProps extends ComponentProps<'div'> {
   description?: string
 }
 
-export function Page({ className, title, description, children, ...props }: PageProps) {
+export function Page({ className, title, description, children }: PageProps) {
   const classes = className ? [style.page, className].join(' ') : style.page
   
   return (
-    <div className={classes} {...props}>
+    <motion.div 
+      className={classes}
+      exit={{ opacity: 0 }}
+      transition={{
+        duration: 1,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
       <Header
         className={style['page-header']}
         title={title}
         description={description}
+        initial={{ opacity: 0, scale: .5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 1,
+          ease: [0, 0.71, 0.2, 1.01]
+        }}
       />
 
       <motion.main 
@@ -33,6 +46,6 @@ export function Page({ className, title, description, children, ...props }: Page
       >
         { children }
       </motion.main>
-    </div>
+    </motion.div>
   )
 }
