4dd2eca44793d2f30e70e3b8cc4c1e076c5d4256
Lucas Garieri <lucaspvg1@gmail.com>
2024-11-06 04:18:03 -0300

    ✨ Add animation end to control scrollbar

--- src/hooks/useNavigate.ts
+++ src/hooks/useNavigate.ts
@@ -4,41 +4,47 @@ import { useAnimate, cubicBezier } from 'framer-motion'
 export const useNavigate = (pages: string[]) => {
   const [ currentPage, setCurrentPage ] = useState(0)
   const [ scrollLock, setScrollLock ] = useState(false)
+  const [ animationEnded, setAnimationEnded ] = useState(true)
   const [ scope, animate ] = useAnimate()
 
   const lockScroll = useCallback(() => setScrollLock(true), [])
   const unlockScroll = useCallback(() => setScrollLock(false), [])
+  const startAnimation = useCallback(() => setAnimationEnded(false), [])
+  const endAnimation = useCallback(() => setAnimationEnded(true), [])
 
   const animatePage = useCallback(
     async (pageIndex: number, direction: 'up' | 'down') => {
+      startAnimation()
       const position = direction === 'up' ? '-100vh' : '0'
       const easing = cubicBezier(.8, .05, .45, .95)
       await animate(pages[pageIndex], { y: position }, { duration: .8, ease: easing })
+      endAnimation()
     },
     [animate, pages]
   )
 
   const nextPage = useCallback(async () => {
     if (scrollLock || currentPage >= pages.length - 1) return
+    setCurrentPage((prev) => prev + 1)
     lockScroll()
 
     await animatePage(currentPage, 'up')
-    setCurrentPage((prev) => prev + 1)
     unlockScroll()
   }, [scrollLock, currentPage, pages.length, animatePage, lockScroll, unlockScroll])
 
   const prevPage = useCallback(async () => {
     if (scrollLock || currentPage <= 0) return
+    setCurrentPage((prev) => prev - 1)
     lockScroll()
 
     await animatePage(currentPage - 1, 'down')
-    setCurrentPage((prev) => prev - 1)
     unlockScroll()
   }, [scrollLock, currentPage, animatePage, lockScroll, unlockScroll])
 
   const goToPage = useCallback((pageId: string) => {
     const pageIndex = pages.findIndex((page) => page === pageId)
     if (scrollLock || pageIndex === currentPage || pageIndex < 0 || pageIndex >= pages.length) return
+    setCurrentPage(pageIndex)
     lockScroll()
 
     const animations = []
@@ -47,11 +53,11 @@ export const useNavigate = (pages: string[]) => {
     }
 
     Promise.all(animations).then(() => {
-      setCurrentPage(pageIndex)
+      endAnimation()
       unlockScroll()
     })
   }, [scrollLock, currentPage, pages.length, animatePage, lockScroll, unlockScroll]
   )
 
-  return { scope, currentPage, nextPage, prevPage, goToPage }
+  return { scope, currentPage, animationEnded, nextPage, prevPage, goToPage }
 }
