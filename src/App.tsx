0cb486a96ac6e476dd35cffe5197c9da9ff002e6
Lucas Garieri <lucaspvg1@gmail.com>
2024-12-14 02:34:12 -0300

    ✨ Update TCC Website

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,19 @@ function Download({ fileName }: DownloadProps) {
 
   useEffect(() => {
     const fileUrl = `/tcc-files/${fileName}`
-    const link = document.createElement("a")
+    window.open(fileUrl, "_blank");
 
-    link.href = fileUrl
-    link.download = fileName
-    link.click()
+    // const link = document.createElement("a")
 
-    const timeout = setTimeout(() => {
-      navigate("/tcc")
-    }, 1000);
+    // link.href = fileUrl
+    // link.download = fileName
+    // link.click()
 
-    return () => clearTimeout(timeout)
+    // const timeout = setTimeout(() => {
+    //   navigate("/tcc")
+    // }, 1000);
+
+    // return () => clearTimeout(timeout)
   }, [fileName, navigate])
 
   return null
@@ -40,7 +42,7 @@ function App() {
         <Route path="tcc">
           <Route index element={<TCC appRef={appRef} />} />
           <Route path="download/banner" element={<Download fileName="Banner.pdf" />} />
-          <Route path="download/thesis" element={<Download fileName="Desenvolvimento de uma solução para monitoramento de presença e engajamento para o Cursinho Popular da Escola Politécnica da Universidade de São Paulo.pdf" />} />
+          <Route path="download/thesis" element={<Download fileName="Desenvolvimento_de_uma_solução_para_monitoramento_de_presença_e_engajamento_para_o_Cursinho_Popular_da_Escola_Politécnica_da_Universidade_de_São_Paulo.pdf" />} />
           <Route path="download/release" element={<Download fileName="Press Release.pdf" />} />
         </Route>
         
